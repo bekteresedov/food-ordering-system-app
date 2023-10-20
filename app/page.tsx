40bdc5b926c6f89de17803bbde6e4024af074046
Bekter <besedov347@gmail.com>
2023-10-20 15:39:24 +0300

    ok

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h2>Hello</h2>
+      <h2 className="text-center font-dancing">Hy</h2>
+    </>
+  );
+}
