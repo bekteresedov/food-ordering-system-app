4e2278f0e65cc77c25f74d599789eef4f621fc76
Bekter <besedov347@gmail.com>
2023-11-24 22:17:16 +0300

    footer updated

--- app/components/layout/footer/index.tsx
+++ app/components/layout/footer/index.tsx
@@ -1,14 +1,56 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import Title from "../../UI/title";
-
+import { IFooterResponse } from "@/app/types/admin/IAdminFooter";
+import { get } from "@/app/service/httpService";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa6";
+import Link from "next/link";
 const Footer = () => {
+  const [footer, setFooter] = useState<IFooterResponse>();
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, statusCode } = await get("/footers/get/1");
+      if (statusCode === 200) {
+        setFooter(data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <footer className="bg-green dark:bg-dbg  text-white pt-10 pb-5">
-        <div className="flex flex-col items-center md:items-start md:flex-row  gap-10 md:w-8/12 mx-auto ">
+        <div className="flex flex-col items-center md:items-start md:flex-row  gap-14 md:w-8/12 mx-auto ">
           <ul className="w-10/12 md:w-4/12">
             <li>
               <Title className="text-2xl">Contact Us</Title>
+            </li>
+            <li className="flex items-center text-[13px] font-mont mt-2 gap-1">
+              <FaLocationDot />
+              <Link
+                href={footer?.location || ""}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span className="text-[12.5px]">Location</span>
+              </Link>
+            </li>
+            <li className="flex items-center text-[13px] font-mont mt-1 gap-1">
+              <FaPhone />
+              <Link href={`tel:${footer?.phoneNumber || ""}`}>
+                <span className="text-[12.5px]">
+                  Call {footer?.phoneNumber}
+                </span>
+              </Link>
+            </li>
+            <li className="flex items-center text-[13px] font-mont mt-1 gap-1">
+              <MdEmail />
+              <Link href={`mailto:${footer?.email || ""}`}>
+                <span className="text-[12.5px]">{footer?.email}</span>
+              </Link>
             </li>
           </ul>
           <ul className="w-10/12 md:w-4/12">
@@ -17,13 +59,24 @@ const Footer = () => {
             </li>
             <li>
               <p className="text-[12.5px] font-mont mt-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-                cum dicta accusantium maiores veritatis non tempore sapiente
+                {footer?.description}
               </p>
             </li>
           </ul>
           <ul className="w-10/12 md:w-4/12">
-            <Title className="text-2xl">Opening Hours</Title>
+            <li>
+              <Title className="text-2xl">Opening Hours</Title>
+            </li>
+            <li>
+              <span className="text-[12.5px] font-mont mt-2">
+                {footer?.openingDay}
+              </span>
+            </li>
+            <li>
+              <span className="text-[12.5px] font-mont mt-2">
+                {footer?.openingHour}
+              </span>
+            </li>
           </ul>
         </div>
         <p className="text-center mt-10 font-semibold text-xs cursor-pointer hover:text-red transition-all">
